a7ed1696dab295e0e102d0d3ec2bdf0ec70abae8
meganbarn-13 <mbarnhart@luc.edu>
2025-01-07 14:23:29 -0600

    organize

--- R_scripts/raw_data_manipulation.R
+++ R_scripts/raw_data_manipulation.R
@@ -5,30 +5,27 @@ library(dplyr)
 data <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/raw_data_files/average-precipitation-per-year.csv")
 state_list <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/list_of_states.csv")
 
-# remove extra states
+# identify states outside of scope
 extra_states <- anti_join(data, state_list, by = "Entity")
-write.csv(extra_states, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/removed_states.csv")
+write.csv(extra_states, "removed_states.csv")
 
+# remove states outside of scope
 precip_data <- data %>%
-  right_join(state_list, by = "Entity") 
-write.csv(precip_data, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/filtered_precip_data.csv")
+  right_join(state_list, by = 'Entity')
+write.csv(precip_data, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/filtered_precip_data.csv")
 
-removed_states <- setdiff(state_list$Entity, weighted_deviation$Entity)
-print(removed_states)
-
-# split data into baseline and current sets
 data_1950_1980 <- precip_data %>%
   filter(Year >= 1950 & Year <= 1980)
 
 data_1982_2023 <- precip_data %>%
   filter(Year >= 1982 & Year <= 2023)
 
-write.csv(data_1950_1980, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/baseline_precip.csv", row.names = FALSE)
-write.csv(data_1982_2023, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/current_precip.csv", row.names = FALSE)
+write.csv(data_1950_1980, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/baseline_precip.csv", row.names = FALSE)
+write.csv(data_1982_2023, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/current_precip.csv", row.names = FALSE)
 
 # calculate abs value weighted deviation from 1950-80 baseline ====
-baseline_precip <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/baseline_precip.csv")
-current_precip <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_intermediate_files/current_precip.csv")
+baseline_precip <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/baseline_precip.csv")
+current_precip <- read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/current_precip.csv")
 
 # baseline average
 baseline_avg <- baseline_precip %>%
@@ -51,4 +48,9 @@ weighted_deviation <- deviation_data %>%
     avg_weighted_deviation = mean(abs_deviation, na.rm = TRUE)
   )
 
-write.csv(weighted_deviation, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/final_indicator_files/precip_weighted_dev.csv", row.names = FALSE)
+write.csv(weighted_deviation, "/Users/home/Documents/Westphalia-Code-II/Westphalia-II/precip_weighted_dev.csv", row.names = FALSE)
+
+missing_states <- setdiff(state_list$Entity, weighted_deviation$Entity)
+cat("states in list missing from deviations:\n")
+print(missing_states)
+# 
