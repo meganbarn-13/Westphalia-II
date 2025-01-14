e890f326007f72e9eb12b6d696789ebc8ddf01a6
meganbarn-13 <mbarnhart@luc.edu>
2025-01-14 17:13:20 -0600

    update multivar_regression

--- R_scripts/multivar_regression_script.R
+++ R_scripts/multivar_regression_script.R
@@ -1,7 +1,9 @@
 # libraries ====
 library(ggcorrplot)
+library(ggplot2)
 library(moments)
 library(ggpubr)
+library(car)
 
 # check data ====
 state_data = read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/values_for_regression.csv")
@@ -11,9 +13,6 @@ names(state_data)
 
 min(state_data$precip_dev)
 max(state_data$precip_dev)
-
-plot(state_data$temp_dev, state_data$problem_solving_score)
-plot(state_data$precip_dev, state_data$problem_solvng_score)
 
 # distributions
 hist(state_data$temp_dev) # positive skew
@@ -27,14 +26,6 @@ hist(state_data$problem_solving_score) # negative skew
 state_data$temp_dev <- sqrt(state_data$temp_dev)
 state_data$precip_dev <- sqrt(state_data$precip_dev)
 state_data$problem_solving_score <- sqrt(max(state_data$problem_solving_score + 1) - state_data$problem_solving_score)
-  
-
-
-# multicollinearity
-trimmed_data <- subset(state_data, select = -problem_solving_score + Entity) # remove problem solving score and nonnumeric column
-corr_matrix = round(cor(trimmed_data), 2) # compute correlation at 2 decimal places
-ggcorrplot(corr_matrix, hc.order = TRUE, type = "lower", lab = TRUE)
-
 
 # multivariate regression model ====
 env_model = lm(formula = problem_solving_score ~ temp_dev + precip_dev,
@@ -46,5 +37,22 @@ residuals = env_model$residuals
 hist(residuals)
 qqnorm(residuals)
 qqline(residuals)
+
+rstandard(env_model)
+plot(env_model, which = 1, main = "Standardized Residuals Plot")
+
+# multicollinearity
+trimmed_data <- subset(state_data, select = -problem_solving_score + Entity) # remove problem solving score and nonnumeric column
+corr_matrix = round(cor(trimmed_data), 2) # compute correlation at 2 decimal places
+ggcorrplot(corr_matrix, hc.order = TRUE, type = "lower", lab = TRUE)
+vif(env_model) # temp VIF: 1.11558 // precip VIF: 1.1158
+
+plot(trimmed_data$temp_dev, trimmed_data$problem_solving_score)
+plot(trimmed_data$precip_dev, trimmed_data$problem_solvng_score)
+
+# durbin-watson test for autocorrelation
+durbinWatsonTest(env_model) # test statistic = 1.772929 // p-value = 0.13
+  # p > 0.05, accept H0 and conclude residuals are not autocorrelated
+
 
 
