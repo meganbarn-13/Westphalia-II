dd8a5f650f308e5b22baf7ca9f6e3cf8e0db46ad
meganbarn-13 <mbarnhart@luc.edu>
2025-01-13 20:42:37 -0600

    Create multivar_regression_script.R

---
+++ R_scripts/multivar_regression_script.R
@@ -0,0 +1,25 @@
+# libraries ====
+library(ggcorrplot)
+
+# multivariate regression model ====
+state_data = read.csv("/Users/home/Documents/Westphalia-Code-II/Westphalia-II/values_for_regression.csv")
+head(state_data)
+dim(churn_data)
+
+env_model = lm(formula = problem_solving_score ~ temp_dev + precip_dev, 
+               data = state_data)
+summary(env_model) # print result
+
+# check assumptions ====
+residuals = env_model$residuals
+hist(residuals)
+qqnorm(residuals)
+qqline(residuals)
+
+# multicollinearity
+trimmed_data <- subset(state_data, select = -problem_solving_score) # remove problem solving score
+corr_matrix = round(cor(trimmed_data), 2) # compute correlation at 2 decimal places
+ggcorrplot(corr_matrix, hc.order = TRUE, type = "lower", lab = TRUE)
+
+
+
